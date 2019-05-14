604449bacd38381783e6f90ffe86f1b823b50149
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2019-05-14 17:06:04 -0300

    automated commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e76e38597bce4b64ca9f61c235c461c9.js"
+  "/precache-manifest.daf5f6867976e051f3a9c2a4c490144f.js"
 );
 
 self.addEventListener('message', (event) => {
